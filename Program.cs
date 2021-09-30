adfd5bbc42b8060eb8da2f9934e10d0a3fb299eb
morkovkalove <73894426+morkovkalove@users.noreply.github.com>
2021-09-30 23:13:24 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -25,32 +25,33 @@ namespace ConsoleApp2
 
             Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
+            
             var color = Console.ReadLine();
 
-            if (color == "red")
+            switch(color)
             {
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.Black;
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
 
-                Console.WriteLine("Your color is red!");
+                    Console.WriteLine("Your color is red!");
+                break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                break;
+
+                default:
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
             }
-
-            else if (color == "green")
-            {
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.ForegroundColor = ConsoleColor.Black;
-
-                Console.WriteLine("Your color is green!");
-            }
-            else
-            {
-                Console.BackgroundColor = ConsoleColor.Cyan;
-                Console.ForegroundColor = ConsoleColor.Black;
-
-                Console.WriteLine("Your color is cyan!");
-
-                Console.ReadKey();
-            }
+            
         }
     }
 }
