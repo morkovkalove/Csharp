4af81352d4961f95a70668774ede830c66df816e
morkovkalove <73894426+morkovkalove@users.noreply.github.com>
2021-12-15 16:25:22 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,64 +1,80 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 
-namespace ConsoleApp2
+namespace ConsoleApp1
 {
     class Program
     {
+        static string ShowColor(string username, int userage)
+        {
+            Console.WriteLine("{0}, {1} лет \nНапишите свой любимый цвет на английском с маленькой буквы", username, userage);
+            var color = Console.ReadLine();
+
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is yellow!");
+                    break;
+            }
+            return color;
+        }
+
         static void Main(string[] args)
         {
-            var (name, age) = ("Евгения", 27);
+            var(name, age) = ("Андрей", 26);
+            //Console.WriteLine("Мое имя : {0}", name);
+            //Console.WriteLine("Мой возраст : {0}", age);
 
-            Console.WriteLine("Мое имя: {0}", name);
-            Console.WriteLine("Мой возраст: {0}", age);
-
-            Console.Write("Введите имя: ");
+            Console.Write("Введите имя:");
             name = Console.ReadLine();
-            Console.Write("Введите возрас с цифрами:");
+
+            Console.Write("Введите возраст цифрами :");
             age = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Ваше имя: {0}", name);
-            Console.WriteLine("Ваш возраст: {0}", age);
+            Console.WriteLine("Ваше имя : {0}", name);
+            Console.WriteLine("Ваш возраст : {0}", age);
 
-            ShowColor();
+            var favcolors = new string[3];
+            
+            for (int i = 0; i < favcolors.Length; i++)
+            {
+                favcolors[i] = ShowColor(name, age);
+            }
 
-	 {
-          
-        static void ShowColor() {
-                
-	Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-        var color = Console.ReadLine();
+            Console.WriteLine("Ваши любимые цвета: ");
+            foreach(var color in favcolors)
+            {
+                Console.WriteLine(color);
+            }
 
-        switch (color) {
-        case "red":
-          Console.BackgroundColor = ConsoleColor.Red;
-          Console.ForegroundColor = ConsoleColor.Black;
+            Console.ReadKey();
 
-          Console.WriteLine("Your color is red!");
-          break;
-
-        case "green":
-          Console.BackgroundColor = ConsoleColor.Green;
-          Console.ForegroundColor = ConsoleColor.Black;
-
-          Console.WriteLine("Your color is green!");
-          break;
-                
-        case "cyan":
-          Console.BackgroundColor = ConsoleColor.Cyan;
-          Console.ForegroundColor = ConsoleColor.Black;
-
-          Console.WriteLine("Your color is cyan!");
-          break;
-                
-        default:
-          Console.BackgroundColor = ConsoleColor.Yellow;
-          Console.ForegroundColor = ConsoleColor.Red;
-
-          Console.WriteLine("Your color is yellow!");
-          break;
-			
+            //ShowColor();
         }
-     }
-	
- }
-        
+    }
+}
